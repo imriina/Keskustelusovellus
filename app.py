faa80086f5c764a9608c77d652f686244990945e
Riina Immonen <riina.immonen@helsinki.fi>
2024-01-17 13:00:29 +0200

    update readme

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-from flask import Flask
 from flask import render_template, request
+from flask import Flask
+
 app = Flask(__name__)
 
-
-@app.route("/form")
-def form():
-    return render_template("form.html")
-
-@app.route("/result", methods=["POST"])
-def result():
-    return render_template("result.html", name=request.form["name"], sname=request.form["sname"])
+@app.route("/")
+def index():
+    return render_template("index.html")
